6f0136128b2065f076dd0722a6246a7ceae4ea3f
pauladixon <paulapdixon@gmail.com>
2020-06-20 21:39:23 -0700

    added service search bar formatting and styling

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,15 +1,92 @@
-import React from 'react';
+import React, { Component } from 'react'
+import Select from 'react-select'
+import serviceCategories from '../../category-data'
+import serviceCities from '../../city-data'
+import './SearchBar.css'
 
-const SearchBar = (props) => {
+class SearchBar extends Component {
 
-  return (
-    <div className="search-bar">
-        search by location
-        <br></br>
-        search by category
-    </div>
+    state = {
+        formData: {
+            city: '',
+            categories: ''
+        }
+    }
 
-  );
-};
+    formRef = React.createRef()
 
-export default SearchBar;
+    handleChangeCategories = categories => {
+        this.setState({ formData: { ...this.state.formData, categories } })
+    }
+
+    handleChangeCities = cities => {
+        this.setState({ formData: { ...this.state.formData, cities } })
+    }
+
+    handleSubmit = e => {
+      e.preventDefault()
+      this.props.handleSearch(this.state.formData)
+    }
+
+    render() {
+        return (
+            <div className="search-bar">
+                <div className='search-item'>
+                    <form 
+                        ref={this.formRef} 
+                        autoComplete="off" 
+                        onSubmit={this.handleSubmit}
+                    >
+                        <label className='search-label'>Search By City :: </label>
+                        <div className='search-row'>
+                            <Select
+                                className='search-select'
+                                value={this.state.formData.cities}
+                                name="cities"
+                                isMulti
+                                onChange={this.handleChangeCities}
+                                options={serviceCities}
+                                required
+                            />
+                            <button
+                                className="search-btn"
+                                type="submit"
+                            >
+                                →
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                <br></br><br></br>
+                <div className='search-item'>
+                    <form 
+                        ref={this.formRef} 
+                        autoComplete="off" 
+                        onSubmit={this.handleSubmit}
+                    >
+                        <label className='search-label'>Search By Category :: </label>
+                        <div className='search-row'>
+                            <Select
+                                className='search-select'
+                                value={this.state.formData.categories}
+                                name="categories"
+                                isMulti
+                                onChange={this.handleChangeCategories}
+                                options={serviceCategories}
+                                required
+                            />
+                            <button
+                                className="search-btn"
+                                type="submit"
+                            >
+                                →
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+          )
+    }
+}
+
+export default SearchBar
