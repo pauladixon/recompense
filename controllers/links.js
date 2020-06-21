3b2bcdff3c39794ba06cfa2dd206482b7bdb181f
isama22 <elisabethmartinez@Elisabeths-MacBook-Pro.local>
2020-06-20 19:47:08 -0700

    pre pull push

--- controllers/links.js
+++ controllers/links.js
@@ -3,9 +3,7 @@ const Link = require('../models/link')
 module.exports = {
     index,
     show, 
-    create,
-    // update,
-    // delete: deleteOne
+    create
 }
 
 async function index(req, res) {
@@ -24,13 +22,3 @@ async function create(req, res) {
     const link = await Link.create(req.body)
     res.status(201).json(link)
 }
-
-// async function update(req, res) {
-//     const updatedService = await Service.findByIdAndUpdate(req.params.id, req.body, {new:true})
-//     res.status(200).json(updatedService)
-// }
-
-// async function deleteOne(req, res) {
-//     const deletedService = await Service.findByIdAndRemove(req.params.id)
-//     res.status(200).json(deletedService)
-// }
