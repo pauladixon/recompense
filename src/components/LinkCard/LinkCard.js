4599f4e0d3b098141abd27c2f5cbc597b4960afe
isama22 <elisabethmartinez@Elisabeths-MacBook-Pro.local>
2020-06-21 18:10:07 -0700

    styled link detail page and card

--- src/components/LinkCard/LinkCard.js
+++ src/components/LinkCard/LinkCard.js
@@ -7,39 +7,71 @@ function LinkCard({ link, handleDeleteLink, user }) {
     user = user === null ? user = NaN : user
     const categories = link.categories.map((category, idx) =>
         <span key={category.value}>&nbsp;&nbsp;{idx !== 0 ? '•' : ''}&nbsp;&nbsp;{category.label}</span>)
+    const cities = link.cities.map((city, idx) =>
+        <span key={city.value}>&nbsp;&nbsp;{idx !== 0 ? '•' : ''}&nbsp;&nbsp;{city.label}</span>)
 
     return (
         <>
             <div className='detail-card'>
                 <div>
-                    <h4>{link.name}</h4>
-                    <h4>link card</h4>
+                    <p className='card-name'>{link.name}</p>
                 </div>
                 <div>
-                    <div>Description: {link.description}</div>
-                    <div>cashapp: {link.cashapp}</div>
-                    <div>venmo: {link.venmo}</div>
-                    <div>paypal: {link.paypal}</div>
+                    <div>
                     <div className='line-item'>
-                            <span className='request-label'>Categories :: </span>
-                            <span className='request-info categories'>{categories}</span>
-                        </div>
+                        <span className='request-label'>Description :: </span>
+                        <span className='request-info'>{link.description}</span>
+                    </div>
+                    <div className='line-item'>
+                        <span className='request-label'>CashApp :: </span>
+                        <span className='request-info'>{link.cashapp}</span>
+                    </div>
+                    <div className='line-item'>
+                        <span className='request-label'>Venmo :: </span>
+                        <span className='request-info'>{link.venmo}</span>
+                    </div>
+                    <div className='line-item'>
+                        <span className='request-label'>PayPal :: </span>
+                        <span className='request-info'>{link.paypal}</span>
+                    </div>
+                    <div className='line-item'>
+                        <span className='request-label'>Categories :: </span>
+                        <span className='request-info categories'>{categories}</span>
+                    </div>
+                    <div className='line-item'>
+                        <span className='request-label'>City, State :: </span>
+                        <span className='request-info'>{cities}</span>
+                    </div>
                     {user._id &&
-                        <div>Contact: {link.creator} at {link.contactEmail}</div>
+                        <div className='line-item'>
+                            <span className='request-label'>Contact :: </span>
+                            <span className='request-info'>{link.creator} at {link.contactEmail}</span>
+                        </div>
                     }
                 </div>
-                <div>
+                </div>
+                <div className='card-links'>
                     {user._id === link.user &&
-                        <Link to={{
-                            pathname: '/edit',
-                            state: { link },
-                        }}>Edit</Link>
-                    } {user._id === link.user &&
                         <button
+                            className='delete'
                             onClick={() => handleDeleteLink(link._id)}
                         >
-                            Delete
+                            Remove
                         </button>
+                    }
+                    <Link
+                        className='home-link'
+                        to='/directaidlinks'
+                    >
+                        Return to Links Page
+                        </Link>
+                    {user._id === link.user &&
+                        <Link
+                            className='edit'
+                            to={{
+                                pathname: '/editlink',
+                                state: { link },
+                            }}>Edit</Link>
                     }
                 </div>
             </div>
--- src/pages/LinkDetailPage/LinkDetailPage.js
+++ src/pages/LinkDetailPage/LinkDetailPage.js
@@ -5,13 +5,29 @@ function LinkDetailPage (props) {
     const link = props.location.state.link
     return (
         <>
-            <h1>Link Details</h1>
+            <div className='post-detail'>
             <LinkCard
                 key={link._id}
                 link={link}
                 user={props.user}
                 handleDeleteLink={props.handleDeleteLink}
             />
+            </div>
+            <div className='break-container'>
+                <div className="break"></div>
+            </div>
+            <div className='title-container'>
+                <p className="comment-title">Comments:</p>          
+            </div>
+            <div className="comments-form">
+
+                <form>
+                    <input>
+
+                    </input>
+                    <button>+</button>
+                </form>
+            </div>
         </>
     )
 }
