fd3eb1c7c9ddbadc88607068bec2bfe2158e959d
isama22 <elisabethmartinez@Elisabeths-MacBook-Pro.local>
2020-06-21 17:49:32 -0700

    pre pull

--- src/components/AddLinkPage/AddLinkPage.js
+++ src/components/AddLinkPage/AddLinkPage.js
@@ -1,30 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import Select from 'react-select';
-import serviceCategories from '../../data';
+import Select from 'react-select'
+import serviceCategories from '../../category-data'
 import serviceCities from '../../city-data'
+// import './AddServicePage.css'
 
-class AddLinkPage extends Component {
+class AddLink extends Component {
   state = {
     invalidForm: true,
     formData: {
       name: '',
+      categories: '',
       description: '',
-      cashapp: '',
       venmo: '',
+      cashapp: '',
       paypal: '',
-      contactEmail: '',
       city: '',
-      categories: ''
+      contactEmail: ''
     }
   }
+
   formRef = React.createRef()
 
   handleSubmit = e => {
     e.preventDefault()
     this.props.handleAddLink(this.state.formData)
   }
-  
+
   handleChange = e => {
     const formData = { ...this.state.formData, [e.target.name]: e.target.value, }
     this.setState({
@@ -42,68 +44,81 @@ class AddLinkPage extends Component {
   }
 
   render() {
-
     return (
-      <div className="Home">
-        <p>add link</p>
-        <form ref={this.formRef} autoComplete="off" onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label>Name</label>
-            <input className="signup-form"
-              name="name"
-              value={this.state.formData.name}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label>Description: </label>
-            <input
-              className="signup-form"
-              name="description"
-              value={this.state.formData.description}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label>CashApp: </label>
-            <input className="signup-form"
-              name="cashapp"
-              value={this.state.formData.cashapp}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label>Venmo: </label>
-            <input className="signup-form"
-              name="venmo"
-              value={this.state.formData.venmo}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label>Paypal: </label>
-            <input className="signup-form"
-              name="paypal"
-              value={this.state.formData.paypal}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label>Contact Email: </label>
-            <input className="signup-form"
-              name="contactEmail"
-              value={this.state.formData.contactEmail}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <div className='form-item'>
-              <label className='request-label'>Categories :: </label>
+      <div className="add-request-page">
+        <div className="page-header">
+          <p className='page-title'>Add a Link :: </p>
+        </div>
+        <div className='add-request-form'>
+          <form 
+            ref={this.formRef} 
+            autoComplete="off" 
+            onSubmit={this.handleSubmit}
+          >
+            <div className='form-item'>
+              <label className='request-label'>Link Name :: </label>
+              <input
+                className="request-form"
+                name="name"
+                value={this.state.formData.name}
+                onChange={this.handleChange}
+                required
+              /> 
+            </div>
+            <div className='form-item'>
+              <label className='request-label'>Description of Link :: </label>
+              <textarea 
+                className="request-form description"
+                name="description"
+                value={this.state.formData.description}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <div className='form-item'>
+              <label className='request-label'>Venmo :: </label>
+              <input 
+                className="request-form"
+                name="venmo"
+                value={this.state.formData.venmo}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <div className='form-item'>
+              <label className='request-label'>Cashapp :: </label>
+              <input 
+                className="request-form"
+                name="cashapp"
+                value={this.state.formData.cashapp}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <div className='form-item'>
+              <label className='request-label'>PayPal :: </label>
+              <input 
+                className="request-form"
+                name="paypal"
+                value={this.state.formData.paypal}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <div className='form-item'>
+              <label className='request-label'>City, State :: </label>
+              <Select
+                className='request-categories'
+                value={this.state.formData.cities}
+                isMulti
+                name="cities"
+                onChange={this.handleChangeCities}
+                options={serviceCities}
+                required
+              />
+            </div>
+            <div className='form-item'>
+              <label className='request-label'>Categories of Link :: </label>
               <Select
                 className='request-categories'
                 value={this.state.formData.categories}
@@ -114,31 +129,31 @@ class AddLinkPage extends Component {
                 required
               />
             </div>
-          <div className='form-item'>
-            <label className='request-label'>City, State :: </label>
-            <Select
-              className='request-categories'
-              value={this.state.formData.cities}
-              isMulti
-              name="cities"
-              onChange={this.handleChangeCities}
-              options={serviceCities}
-              required
-            />
-          </div>
-          <div className="add-links">
-          <Link className='cancel-button' to='/links'>Cancel</Link>
-          <button
-            className="btn"
-            type="submit"
-            disabled={this.state.invalidForm}
-          >
-            Add Link
-              </button>
+            <div className='form-item'>
+              <label className='request-label'>Contact Email :: </label>
+              <input 
+                className="request-form"
+                name="contactEmail"
+                value={this.state.formData.contactEmail}
+                onChange={this.handleChange}
+                required
+              />
             </div>
-        </form>
+            <div className='add-links'>
+                <Link className='cancel-button' to='/links'>Cancel</Link>
+                <button
+                    className="btn"
+                    type="submit"
+                    disabled={this.state.invalidForm}
+                >
+                    Add Link
+                </button>
+            </div>
+          </form>
+        </div>
       </div>
-    );
+    )
   }
 }
-export default AddLinkPage;
+
+export default AddLink
