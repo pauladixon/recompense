afb5840b5134b09c088c0c736bfd8f23b3ccfd48
pauladixon <paulapdixon@gmail.com>
2020-06-30 16:16:34 -0700

    home page styling tweaks

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,8 +9,8 @@ const Home = (props) => {
           <div className="home-page">
             <div className="home-text">
               <div className="home-p">
-                <p>Recompense • as a verb, means to make amends for loss or harm suffered; to compensate.</p>
-
+                <p><span className='page-title'>Recompense</span> :: as a verb, means to make amends for loss or harm suffered; to compensate.</p>
+                <br></br>
                 <p>Recompense is a community driven application for paying reparations. Users can post services to offer, make service requests, and donate to individuals in need. In the ongoing effort to redistribute resources and care, we hope this application will help further the task of connecting individuals to resources that they need, and the resources that individuals can provide to others.</p>
 
                 <p>Included <a className="doc-link" href="https://docs.google.com/document/d/1z9F3SPp-Ft_9kCWLB_TE-1Nl_jSaQnkAwvFEy4HTQqs/edit" target="_blank" rel="noopener noreferrer">here</a> is a google doc library of resources we’ve compiled, aimed mostly at the national, and major city level, for continuing to provide assistance outside of our site.</p>
