8e11a98bf0a84d93a0cab4521af3a9ee551b75fe
pauladixon <paulapdixon@gmail.com>
2020-07-16 22:46:41 -0700

    changed favicon

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
                 <br></br>
                 <p>Recompense is a community driven application for paying reparations. Users can post services to offer, make service requests, and donate to individuals in need. In the ongoing effort to redistribute resources and care, we hope this application will help further the task of connecting individuals to resources that they need, and the resources that individuals can provide to others.</p>
 
-                <p>Included <a className="doc-link" href="https://docs.google.com/document/d/1z9F3SPp-Ft_9kCWLB_TE-1Nl_jSaQnkAwvFEy4HTQqs/edit" target="_blank" rel="noopener noreferrer">here</a> is a google doc library of resources we’ve compiled, aimed mostly at the national, and major city level, for continuing to provide assistance outside of our site.</p>
+                <p>Included <a className="doc-link" href="http://pfw.guide/" target="_blank" rel="noopener noreferrer">here</a> is the Master List of resources compiled by Patia's Fantasy World, aimed mostly at the national, and major city level, for continuing to provide assistance outside of our site.</p>
               </div>
       </div>
     </div>
