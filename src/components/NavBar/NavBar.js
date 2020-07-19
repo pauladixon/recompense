486089cf030aa96763e2ad61ae4d6b779c08c76f
pauladixon <paulapdixon@gmail.com>
2020-07-18 23:58:32 -0700

    added animation to home page - testing for deployment

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,12 +3,18 @@ import { Link } from 'react-router-dom'
 import './NavBar.css'
 
 const NavBar = (props) => {
-  let nav = props.user ?
+  let nav1 = props.user ?
     <div >
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <span className='NavBar-welcome'>Welcome, {props.user.name}</span>
+    </div>
+    :
+    <div>
+    </div>
+  let nav2 = props.user ?
+    <div >
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <Link className='nav' to='' onClick={props.handleLogout} >Log Out</Link>
+      <Link className='nav' to='' onClick={props.handleLogout} >Sign Out</Link>
       &nbsp;
     </div>
     :
@@ -17,17 +23,17 @@ const NavBar = (props) => {
       <Link className='nav' to='/login'>Log In</Link>
       &nbsp;&nbsp;&nbsp;&nbsp;
       <Link className='nav' to='/signup'>Sign Up</Link>
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     </div>
   return (
     <div className="nav-bar">
+      {nav1}
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <Link className="nav" to="/directaidlinks">Direct Aid</Link>
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <Link className="nav" to="/servicesfloor">Service Floor</Link>
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <Link className="nav" to="/requests">Request Floor</Link>
-      {nav}
+      {nav2}
     </div>
 
   )
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,14 +5,16 @@ import Footer from '../../components/Footer/Footer'
 const Home = () => {
   return (
     <div className="Home">
-      <div className="home-border-1">
-          <div className="home-p">
-            <p><span className='page-title'>Recompense</span> :: as a verb, means to make amends for loss or harm suffered; to compensate.</p>
-            <br></br>
-            <p>Recompense is a community driven application for paying reparations to BIPOC. Users can post services to offer, make service requests, and donate to individuals in need. In the ongoing effort to redistribute resources and care, we hope this application will help further the task of connecting individuals to resources that they need, and the resources that individuals can provide to others.</p>
+      <div className="home-border-1 x">
+        <div className="home-border-2 x">
+            <div className="home-p">
+              <p><span className='page-title'>Recompense</span> :: as a verb, means to make amends for loss or harm suffered; to compensate.</p>
+              {/* <br></br> */}
+              <p>Recompense is a community driven application for paying reparations to BIPOC. Users can post services to offer, make service requests, and donate to individuals in need. In the ongoing effort to redistribute resources and care, we hope this application will help further the task of connecting individuals to resources that they need, and the resources that individuals can provide to others.</p>
 
-            <p>Included <a className="doc-link" href="http://pfw.guide/" target="_blank" rel="noopener noreferrer">here</a> is a link to the master list of resources compiled by Patia's Fantasy World for the continued support of the Black Lives Matter movement outside of our site.</p>
-          </div>
+              <p>Included <a className="doc-link" href="http://pfw.guide/" target="_blank" rel="noopener noreferrer">here</a> is a link to the master list of resources compiled by Patia's Fantasy World for the continued support of the Black Lives Matter movement outside of our site.</p>
+            </div>
+        </div>
       </div>
       <footer>
           <Footer/>
