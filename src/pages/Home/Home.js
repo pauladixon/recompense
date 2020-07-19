add1d1c9bcbce910b2424c5de7ec40dc6f90482b
pauladixon <paulapdixon@gmail.com>
2020-07-19 00:36:10 -0700

    adjusted animation and colors

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import './Home.css';
+import React, {Component} from 'react'
+import './Home.css'
 import Footer from '../../components/Footer/Footer'
 
-const Home = () => {
-  return (
-    <div className="Home">
-      <div className="home-border-1 x">
+class Home extends Component {
+
+  render () {
+    return (
+      <div className="Home">
         <div className="home-border-2 x">
             <div className="home-p">
               <p><span className='page-title'>Recompense</span> :: as a verb, means to make amends for loss or harm suffered; to compensate.</p>
               {/* <br></br> */}
               <p>Recompense is a community driven application for paying reparations to BIPOC. Users can post services to offer, make service requests, and donate to individuals in need. In the ongoing effort to redistribute resources and care, we hope this application will help further the task of connecting individuals to resources that they need, and the resources that individuals can provide to others.</p>
-
+  
               <p>Included <a className="doc-link" href="http://pfw.guide/" target="_blank" rel="noopener noreferrer">here</a> is a link to the master list of resources compiled by Patia's Fantasy World for the continued support of the Black Lives Matter movement outside of our site.</p>
             </div>
         </div>
+        <div className="home-border-1 y">
+        </div>
+        <footer>
+            <Footer/>
+        </footer>
       </div>
-      <footer>
-          <Footer/>
-      </footer>
-    </div>
-  );
+    )
+  }
 }
-export default Home;
+export default Home
