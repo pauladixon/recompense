a49ec239652e84e6926b90d6407ae0d53d8ca0eb
pauladixon <paulapdixon@gmail.com>
2020-07-30 20:01:43 -0700

    mobile home page test

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,19 +6,9 @@ const Footer = (props) => {
 
   return (
     <div className="footer-bar">
-        &nbsp;
-        <p className="mobile-foo">Ⓒ Paula Dixon & Isa Martinez, 2020</p>
-        <p className="foo">Copyright Ⓒ Paula Dixon & Isa Martinez, 2020</p>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        {/* <a 
-          className="foo-link" 
-          href="mailto:isasofma@gmail.com paulapdixon@gmail.com" 
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Contact Us
-        </a> */}
-        <Link className="foo-link" to="/contact">Contact us</Link>
+      <p className="mobile-foo">Ⓒ Paula Dixon & Isa Martinez, 2020</p>
+      <p className="foo">Copyright Ⓒ Paula Dixon & Isa Martinez, 2020</p>
+      <Link className="foo-link" to="/contact">Contact us</Link>
     </div>
   )
 }
